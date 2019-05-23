3b743c736f64201de406ae4bfad8e73126316197
RichardF12 <00168117@uca.edu.sv>
2019-05-22 22:50:27 -0600

    Implementacion inicial de Repository

---
+++ app/src/main/java/com/example/biblioteca/repository/BookRepository.kt
@@ -0,0 +1,15 @@
+package com.example.biblioteca.repository
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+/*
+class BookRepository(private val bookDao: BookDao) {
+
+    val allBooks: LiveData<List<Book>> = BookDao.getAllBooks()
+    // val favBooks: LiveData<List<Book>> = BookDao.getFavBooks()
+
+    @WorkerThread
+    suspend fun insert(book: Book) {
+        bookDao.insert(book)
+    }
+}*/
---
+++ app/src/main/java/com/example/biblioteca/repository/TagRepository.kt
@@ -0,0 +1,14 @@
+package com.example.biblioteca.repository
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+/*
+class TagRepository(private val tagDao: TagDao) {
+
+    val allTags: LiveData<List<Tag>> = tagDao.getAllTags
+
+    @WorkerThread
+    suspend fun insert(tag: Tag) {
+        tagDao.insertTag(tag)
+    }
+}*/
