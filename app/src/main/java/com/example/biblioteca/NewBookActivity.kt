497e513e7da1aa5a72424b80c1b998356f383a71
Carlos Paredes <00028516@uca.edu.sv>
2019-05-25 11:29:34 -0600

    NewBookActivity

--- app/src/main/java/com/example/biblioteca/NewBookActivity.kt
+++ app/src/main/java/com/example/biblioteca/NewBookActivity.kt
@@ -3,14 +3,19 @@ package com.example.biblioteca
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProviders
+import com.example.biblioteca.database.entities.Author
 import com.example.biblioteca.database.entities.Book
+import com.example.biblioteca.database.entities.Tag
 import com.example.biblioteca.model.BookViewModel
 import kotlinx.android.synthetic.main.activity_new_book.*
+
 
 class NewBookActivity : AppCompatActivity() {
 
     private lateinit var bookViewModel: BookViewModel
     private lateinit var book : Book
+    private lateinit var author : Author
+    private lateinit var tag : Tag
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,18 +24,25 @@ class NewBookActivity : AppCompatActivity() {
         bookViewModel = ViewModelProviders.of(this).get(BookViewModel::class.java)
 
         bt_add.setOnClickListener {
+            val authors = bookViewModel.getAllAuthor.value?.size
+            val tags = bookViewModel.getAllTag.value?.lastIndex
             book = Book(et_isbn.text.toString(),
                 et_tittle.text.toString(),
                 et_editorial.text.toString(),
-                3,
+                //TODO: Yo queria solo mandar a llamar el total de la lista authors para solo sumarle 1 y que ese sea el nuevo id de mi Author y Tag
+                /*authors*/1,
                 "cover_image",
                 et_summary.text.toString(),
                 false,
-                3
+                1
+                //tags
             )
+            author = Author(/*authors*/1,et_author.text.toString())
+            tag = Tag(/*tags*/1,et_tag.text.toString())
+
             bookViewModel.insertBook(book)
+            bookViewModel.insertAuthor(author)
+            bookViewModel.insertTag(tag)
         }
-
-
     }
 }
